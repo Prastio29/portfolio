f12d6975335a5c69ad5d1be3cb9c2df1f7e95d98
tio <prastio.r290@gmail.com>
2026-09-03 13:26:50 +0700

    BIG UPDATE

---
+++ script.js
@@ -0,0 +1,9 @@
+const body = document.body
+const menuHome = document.getElementById("menu-home")
+const menuAbout = document.getElementById("menu-about")
+const menuWorks = document.getElementById("menu-works")
+const menuContact = document.getElementById("menu-contact")
+
+menuHome.addEventListener("click", () => {
+    menuHome.style.backgroundColor = #A8C5E0;
+}) 
